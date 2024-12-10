1b2792e4f2ac5e77d5c8697e7f3de3ccfdf12684
Caden Tedschi <cmt489@nau.edu>
2024-12-09 18:52:03 -0700

    Created file moving branches

---
+++ client/src/components/GroceryList.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function GroceryList() {
+   
+
+  return (
+    <div>GroceryList</div>
+  )
+}
+
+export default GroceryList
